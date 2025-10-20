ef4f63341cffce65a347abca4c89ec2b53534e45
Arin Johar <johararin.com>
2025-10-20 16:16:11 -0700

    on step Organizing your data for Milestone 2

---
+++ milestone2/src/app/blogData.ts
@@ -0,0 +1,27 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+    {
+        title: "Blog 1",
+        date: "Oct 15, 2025",
+        description: "This is the first blog",
+        image: "Flag_of_Canada.svg",
+        imageAlt: "",
+        slug: "blog1.html",
+    },
+    {
+        title: "Blog 2",
+        date: "Oct 15, 2025",
+        description: "This is the second blog",
+        image: "Flag_of_Canada.svg",
+        imageAlt: "",
+        slug: "blog2.html",
+    },
+];
